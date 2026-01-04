7c2f2b7a674ff8631510058a0e3e484d22c10507
MCK1989 <m.cortellazzi2@gmail.com>
2026-01-04 15:51:59 +0100

    Initial Commit

---
+++ MemIT/MemITApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemITApp.swift
+//  MemIT
+//
+//  Created by Marco Cortellazzi on 04/01/26.
+//
+
+import SwiftUI
+
+@main
+struct MemITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
